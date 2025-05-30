b627c31dafd68f5137bd7bf0aec15fdfdd54da80
Dqn13lMora1s <dqnielmorais2002@gmail.com>
2025-05-30 10:33:01 +0100

    frontend + algumas outras coisasa

---
+++ frontend/new.js
@@ -0,0 +1,15 @@
+
+const listanomes = document.getElementById("listanomes");
+
+async function mostranomes() {
+    listanomes.innerHTML = "";
+    resposta = await fetch("https://restapi-deploy-demo-b7u0.onrender.com/nomes");
+    nomes = await resposta.json();
+    for (cadanome of nomes) {
+        novop = document.createElement("p");
+        novop.innerHTML = cadanome.nome;
+        listanomes.appendChild(novop);
+    }
+}
+
+mostranomes();
